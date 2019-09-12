c2015ab7c2e449e9765569578b32c093a2376f9a
ShaanSavani <47604432+ShaanSavani@users.noreply.github.com>
2019-09-12 09:48:22 -0400

    Created arcade template, and home/menu screen

--- rock_scissors_paper.py
+++ rock_scissors_paper.py
@@ -1,11 +1,34 @@
-"""
-Rock, Scissors, Paper
+import arcade
 
-Group members: Shaan, Ethan, Justin, Mateo
-"""
+# screen dimension
+
+WIDTH = 1365
+HEIGHT = 710
+
+# variables
+
+current_screen = "play"
+x = 0
+y = 0
+xx = 0
+
+# open window
+
+arcade.open_window(WIDTH, HEIGHT, "Rock Paper Scissors")
+arcade.set_background_color(arcade.color.WOOD_BROWN)
+
+# render    
+
+arcade.start_render()
+
+# draw
+
+arcade.draw_text("Rock Paper Scissors", HEIGHT - 700, WIDTH / 2, arcade.color.BLACK, font_size=20,font_name="garamond")
 
 
 
-def some_function():
-    return True
 
+
+arcade.finish_render()
+
+arcade.run()
