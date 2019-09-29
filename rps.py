dcc6d9c0cff399fd9c8fe89e067acc6360e1964c
ShaanSavani <47604432+ShaanSavani@users.noreply.github.com>
2019-09-29 15:05:36 -0400

    Updated by Shaan

--- rps.py
+++ rps.py
@@ -1,19 +1,19 @@
 import arcade
 import random
+import math
+from random import randint
 
-WIDTH =  1365
+WIDTH = 1365
 HEIGHT = 710
 
+# Variables
 current_screen = "menu"
-
 x = 0
 y = 0
 xx = 0
 
-
 def update(delta_time):
     pass
-
 
 
 def on_draw():
@@ -42,7 +42,7 @@ def menu_keybinds(key, modifiers):
     global current_screen
     if key == arcade.key.I:
         current_screen = "instructions"
-    elif key == arcade.key.P:
+    elif key == arcade.key.ENTER:
         current_screen = "play"
     elif key == arcade.key.ESCAPE:
         exit()
@@ -58,6 +58,12 @@ def play_keybinds(key, modifiers):
     global current_screen
     if key == arcade.key.ESCAPE:
         current_screen = "menu"
+    if key == arcade.key.R:
+        player = "Pock"
+    if key == arcade.key.P:
+        player = "Paper"
+    if key == arcade.key.S:
+        player = "Scissors"
 
 
 def on_key_release(key, modifiers):
@@ -71,7 +77,7 @@ def on_mouse_press(x, y, button, modifiers):
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "Rock, scissors, paper")
     arcade.set_background_color(arcade.color.WHITE)
-    arcade.schedule(update, 1/60)
+    arcade.schedule(update, 1 / 60)
 
     # Override arcade window methods
     window = arcade.get_window()
@@ -84,30 +90,56 @@ def setup():
 
 
 def draw_menu():
-    arcade.set_background_color(arcade.color.WHITE_SMOKE)
-    arcade.draw_text("Rock, Paper, Scissors", WIDTH/2, HEIGHT/2+100,
-                     arcade.color.BLACK, font_size=30, anchor_x="center")
-    arcade.draw_text("I for Instructions", WIDTH/2, HEIGHT/2-40,
-                     arcade.color.BLACK, font_size=20, anchor_x="center")
-    arcade.draw_text("P to Play", WIDTH/2, HEIGHT/2,
-                     arcade.color.BLACK, font_size=20, anchor_x="center")
+    arcade.set_background_color(arcade.color.BLACK)
+    arcade.draw_text("-- Rock -- Paper -- Scissors --", HEIGHT - 180, WIDTH / 4, arcade.color.WHITE, font_name="garamond", font_size=16)
+    arcade.draw_text("Press Enter To Start", HEIGHT - 105, WIDTH / 4.4, arcade.color.WHITE, font_name="garamond", font_size=10)
+    arcade.draw_text("Justin, Ethan, Mateo, Shaan", HEIGHT / 60, WIDTH / 2, arcade.color.WHITE, font_name="garamond", font_size=10)
+    arcade.draw_text("Press I for info", HEIGHT - 90, WIDTH / 4.8, arcade.color.WHITE, font_name="garamond", font_size=10)
+
 
 def draw_play():
-    arcade.set_background_color(arcade.color.WHITE)
-    
-   
+    arcade.set_background_color(arcade.color.BLACK)
+    arcade.draw_text("Choose your move", HEIGHT - 180, WIDTH / 4, arcade.color.WHITE, font_name="garamond", font_size=16)
+
 def draw_instructions():
-    arcade.set_background_color(arcade.color.WHITE)
-    arcade.draw_text("Instructions", WIDTH/2, HEIGHT/2+100,
-                     arcade.color.BLACK, font_size=30, anchor_x="center")
-    arcade.draw_text("Click a button to choose your selection", WIDTH/2, HEIGHT/2,
-                     arcade.color.BLACK, font_size=20, anchor_x="center")
-    arcade.draw_text("ESC to go back", WIDTH/2, HEIGHT/2-60,
-                     arcade.color.BLACK, font_size=20, anchor_x="center")
+    arcade.set_background_color(arcade.color.BLACK)
+    arcade.draw_text("Instructions", HEIGHT / 60, WIDTH / 2, arcade.color.WHITE, font_name="garamond", font_size=10)
+    arcade.draw_xywh_rectangle_outline(510, 260, 100, 100, arcade.color.WHITE)
+    arcade.draw_text("R", HEIGHT - 155, WIDTH / 4.5, arcade.color.WHITE, font_name = "garamond", font_size = 10)
+    arcade.draw_xywh_rectangle_outline(630, 260, 100, 100, arcade.color.WHITE)
+    arcade.draw_text("P", HEIGHT - 34, WIDTH / 4.5, arcade.color.WHITE, font_name = "garamond", font_size = 10)
+    arcade.draw_xywh_rectangle_outline(750, 260, 100, 100, arcade.color.WHITE)
+    arcade.draw_text("S", HEIGHT + 85, WIDTH / 4.5, arcade.color.WHITE, font_name = "garamond", font_size = 10)
+    arcade.draw_text("Select from the keys:", HEIGHT - 105, WIDTH / 3.6, arcade.color.WHITE, font_name= "garamond", font_size= 14)
+    arcade.draw_text("Press ESC to go back", HEIGHT + 525, WIDTH / 2, arcade.color.WHITE, font_name= "garamond", font_size= 10)
 
-
-   
-    
 
 if __name__ == '__main__':
     setup()
+
+# t = ["Rock", "Paper", "Scissors"]
+#     computer = t[randint(0, 2)]
+#     player = False
+#     while player == False:
+#         if player == computer:
+#             arcade.draw_text("TIE", HEIGHT - 180, WIDTH / 4, arcade.color.WHITE, font_name="garamond", font_size=16)
+#         elif player == "Rock":
+#             if computer == "Paper":
+#                 print("You lose!", computer, "covers", player)
+#             else:
+#                 print("You win!", player, "smashes", computer)
+#         elif player == "Paper":
+#             if computer == "Scissors":
+#                 print("You lose!", computer, "cut", player)
+#             else:
+#                 print("You win!", player, "covers", computer)
+#         elif player == "Scissors":
+#             if computer == "Rock":
+#                 print("You lose...", computer, "smashes", player)
+#             else:
+#                 print("You win!", player, "cut", computer)
+#         else:
+#             print("That's not a valid play. Check your spelling!")
+
+#         player = False
+#         computer = t[randint(0, 2)]
